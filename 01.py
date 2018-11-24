8d2c77c1744a43d6f2fbc2a40ab2cb0ec3145403
shotaro-ooe <shoooe97@gmail.com>
2018-11-24 09:49:14 +0900

    sub 関数

---
+++ 01.py
@@ -0,0 +1,12 @@
+def add(number1, number2):
+    return number1 + number2
+
+
+# number1 = int(input("一つ目の整数を入力して下さい>"))
+# number2 = int(input("二つ目の整数を入力して下さい>"))
+
+# print()
+
+assert 7 == add(3, 4)
+assert 10 == add(2, 8)
+assert 3 == add(5, 4)
---
+++ 02.py
@@ -0,0 +1,10 @@
+# 二つの整数の引き算画数 sub を実装する
+
+def sub(x, y):
+    return x - y
+
+# 5 = 7-2
+assert 5 == sub(7, 2)
+
+# 3 = 10 - 3
+assert 7 == sub(10, 3)
